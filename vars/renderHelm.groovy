d5d0e7692684701c447739d771f5202e85563a67
Yogeshwaran-Palanisamy <yogeshwaranpsp@outlook.com>
2025-12-26 17:45:24 +0530

    Rendering Helm function

---
+++ vars/renderHelm.groovy
@@ -0,0 +1,27 @@
+def call() {
+    sh '''
+    #!/bin/python3
+    import os
+    import subprocess
+    import sys
+    import yaml
+
+    value_file = os.listdir("deploy")
+    for file in value_file:
+        if file.endswith(".yaml"):
+            with open(f"deploy/{file}", 'r') as filename:
+                try:
+                    values = yaml.load(filename, Loader=yaml.FullLoader)
+                    chart = values["metadata"]["chart"]
+                    name = values["metadata"]["name"]
+                    namespace = values["metadata"]["namespace"]
+                    chart_name = chart.split("/")[-1]
+                except yaml.YAMLError as exc:
+                    print(exc)
+        helm_cmd = f'helm template {name} {chart} --namespace {namespace} --create-namespace --values deploy/{file} --output-dir ./out'
+        result = subprocess.run(helm_cmd, shell=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            print(f"Error running helm command: {result.stderr}")
+            sys.exit(1)
+    '''
+}
