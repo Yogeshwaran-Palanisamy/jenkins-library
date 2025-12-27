182f06536d6e4c5c6e076d31bbf041c69ac28758
Yogeshwaran-Palanisamy <yogeshwaranpsp@outlook.com>
2025-12-27 10:28:49 +0530

    Refactor renderHelm.groovy for better readability

--- vars/renderHelm.groovy
+++ vars/renderHelm.groovy
@@ -1,27 +1,27 @@
 def call() {
     sh """#!/usr/local/bin/python3
-    import os
-    import subprocess
-    import sys
-    import yaml
+import os
+import subprocess
+import sys
+import yaml
 
-    value_file = os.listdir("deploy")
-    print(value_file)
-    for file in value_file:
-        if file.endswith(".yaml"):
-            with open(f"deploy/{file}", 'r') as filename:
-                try:
-                    values = yaml.load(filename, Loader=yaml.FullLoader)
-                    chart = values["metadata"]["chart"]
-                    name = values["metadata"]["name"]
-                    namespace = values["metadata"]["namespace"]
-                    chart_name = chart.split("/")[-1]
-                except yaml.YAMLError as exc:
-                    print(exc)
-        helm_cmd = f'helm template {name} {chart} --namespace {namespace} --create-namespace --values deploy/{file} > out/{name}.yaml'
-        result = subprocess.run(helm_cmd, shell=True, capture_output=True, text=True)
-        if result.returncode != 0:
-            print(f"Error running helm command: {result.stderr}")
-            sys.exit(1)
+value_file = os.listdir("deploy")
+print(value_file)
+for file in value_file:
+    if file.endswith(".yaml"):
+        with open(f"deploy/{file}", 'r') as filename:
+            try:
+                values = yaml.load(filename, Loader=yaml.FullLoader)
+                chart = values["metadata"]["chart"]
+                name = values["metadata"]["name"]
+                namespace = values["metadata"]["namespace"]
+                chart_name = chart.split("/")[-1]
+            except yaml.YAMLError as exc:
+                print(exc)
+    helm_cmd = f'helm template {name} {chart} --namespace {namespace} --create-namespace --values deploy/{file} > out/{name}.yaml'
+    result = subprocess.run(helm_cmd, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error running helm command: {result.stderr}")
+        sys.exit(1)
     """
 }
